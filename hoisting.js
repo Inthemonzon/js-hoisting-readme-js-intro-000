01bbc5ab19e69c59f81fe313f634c9f95be94d3b
Deshawn Monzon <inthemonzon@yahoo.com>
2021-05-06 20:29:26 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -18,14 +18,12 @@ function crazy() {
   }
 }
 
+sayMy();
+function sayMy() {
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
 }
+  
